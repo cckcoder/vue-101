ff1a6f97fae35c6e209ec8dddea9843792f43854
cckcoder <c.chaiwat.ch@gmail.com>
2024-07-20 13:51:57 +0700

    update: use option-api for counter app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      count: 5,
+      title: 'My Counter App'
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ title }}</h1>
+    <div class="counter">
+      <button class="btn-counter" v-on:click="count += 2">++</button>
+      <button class="btn-counter" v-on:click="increment">+</button>
+      <h2>{{ count }}</h2>
+      <button class="btn-counter" @click="decrement">-</button>
+      <button class="btn-counter" @click="count -= 2">--</button>
+    </div>
   </main>
 </template>
