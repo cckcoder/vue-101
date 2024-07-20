f33ab5f79eb897eb7e80aea581b766fc3bc4c88b
cckcoder <c.chaiwat.ch@gmail.com>
2024-07-20 15:47:36 +0700

    update: change to composition-api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
-<script>
-export default {
-  data() {
-    return {
-      count: 5,
-      title: 'My Counter App'
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    },
-    decrement() {
-      this.count--
-    }
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const title = ref('My Composition API')
+
+const increment = (number) => {
+  if (number) {
+    count.value += number
+  } else {
+    count.value++
+  }
+}
+
+const decrement = (number) => {
+  if (number) {
+    count.value -= number
+  } else {
+    count.value--
   }
 }
 </script>
@@ -21,11 +25,11 @@ export default {
   <main>
     <h1>{{ title }}</h1>
     <div class="counter">
-      <button class="btn-counter" v-on:click="count += 2">++</button>
-      <button class="btn-counter" v-on:click="increment">+</button>
+      <button class="btn-counter" @click="increment(5)">++</button>
+      <button class="btn-counter" @click="increment()">+</button>
       <h2>{{ count }}</h2>
-      <button class="btn-counter" @click="decrement">-</button>
-      <button class="btn-counter" @click="count -= 2">--</button>
+      <button class="btn-counter" @click="decrement()">-</button>
+      <button class="btn-counter" @click="decrement(2)">--</button>
     </div>
   </main>
 </template>
