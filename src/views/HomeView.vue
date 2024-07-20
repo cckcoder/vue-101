2e03c53a4a9624b7ddfeb995dba32eaed2fdaa1c
cckcoder <c.chaiwat.ch@gmail.com>
2024-07-20 15:56:44 +0700

    add: v-model

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 const count = ref(0)
 const title = ref('My Composition API')
+const searchText = ref('')
 
 const increment = (number) => {
   if (number) {
@@ -30,6 +31,17 @@ const decrement = (number) => {
       <h2>{{ count }}</h2>
       <button class="btn-counter" @click="decrement()">-</button>
       <button class="btn-counter" @click="decrement(2)">--</button>
+    </div>
+    <hr />
+    <div>
+      <label for="title-model">V-model</label>&nbsp;
+      <input v-model="searchText" type="text" name="title-model" />
+      <h3>{{ searchText }}</h3>
+    </div>
+    <div>
+      <label for="title-model">V-model: Counter</label>&nbsp;
+      <input v-model="count" type="text" name="title-model" />
+      <h3>{{ count }}</h3>
     </div>
   </main>
 </template>
