2e7bab5fda3262d01a38558eff9e665d58c25922
cckcoder <c.chaiwat.ch@gmail.com>
2024-07-21 15:39:07 +0700

    add: submit from and bulma css

---
+++ src/views/TodoView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('My Todo List')
+const todos = ref([
+  { id: 1, title: 'Drink Coffee', description: 'Awesome' },
+  { id: 2, title: 'Learn Vue3', description: 'I love vue3' },
+  { id: 3, title: 'Learn Django REST', description: 'Learn Django REST' }
+])
+
+const submitTodoForm = () => {
+  console.log('form submit')
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <form @submit.prevent="submitTodoForm">
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Text input" />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <button>Submit</button>
+    </form>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <p>{{ todo.title }} | {{ todo.description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
